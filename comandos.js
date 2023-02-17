a7c5420afb5a25717ae7941653ddf9c24ee036fe
eduardo <kratosedu18@gmail.com>
2023-02-16 23:31:04 -0600

    segundo cambio, escrtuctura html

--- comandos.js
+++ comandos.js
@@ -1,3 +1,5 @@
 //crear nuevo repositorio: git init
 //ver archivos: git status
 //agregar archivos: git add index.js git add.
+//agregar mensaje para etiquetar la subida: git commit -m 'primer subida'
+//revisar todos los commits del repo: git log --oneline
